247065242b78e4d425877508a4fd87bab8920774
Abel Ananta <abelananta9@gmail.com>
2024-05-14 09:54:29 +0700

    fix async issue 2

--- cdn/wright-talent/jobDetail-page.js
+++ cdn/wright-talent/jobDetail-page.js
@@ -2,90 +2,82 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const jobId = urlParams.get("id");
 
-async () => {
-  if (!jobId) {
-    window.location.href = "https://wright-talent.webflow.io/jobs";
+if (!jobId) {
+  window.location.href = "https://wright-talent.webflow.io/jobs";
+}
+
+let fetchJob = async () => {
+  try {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        authorization:
+          "Basic 813438d5227b9ea7eb3df82ec03b89a69d5585aa486a6cd52add3bb1081270ebcdd7850721798ac47139770f1d0a910af958cf7e543b2a29c6164c446343f446fca0633d8003239a150f86edd7ca693ba2477c72e725ab5202aafbdedcd4b226c31cdefb9aea7540afbae847a8f0aab6dc6248ff6fe507ebd52bec4fd1dc2c99",
+      },
+    };
+
+    const response = await fetch(
+      "https://app.loxo.co/api/wright-talent/jobs/" + jobId,
+      options
+    );
+    const data = await response.json();
+
+    const skeletonElements = document.querySelectorAll("[ms-code-skeleton]");
+
+    skeletonElements.forEach((element) => {
+      // Get delay from the attribute
+      let delay = element.getAttribute("ms-code-skeleton");
+
+      // If attribute value is not a number, set default delay as 2000ms
+      if (isNaN(delay)) {
+        delay = 2000;
+      }
+
+      setTimeout(() => {
+        // Remove the skeleton loader div after delay
+        const skeletonDiv = element.querySelector(".skeleton-loader");
+        element.removeChild(skeletonDiv);
+      }, delay);
+    });
+
+    return data;
+  } catch (error) {
+    return [];
   }
-
-  let fetchJob = async () => {
-    try {
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          authorization:
-            "Basic 813438d5227b9ea7eb3df82ec03b89a69d5585aa486a6cd52add3bb1081270ebcdd7850721798ac47139770f1d0a910af958cf7e543b2a29c6164c446343f446fca0633d8003239a150f86edd7ca693ba2477c72e725ab5202aafbdedcd4b226c31cdefb9aea7540afbae847a8f0aab6dc6248ff6fe507ebd52bec4fd1dc2c99",
-        },
-      };
-
-      const response = await fetch(
-        "https://app.loxo.co/api/wright-talent/jobs/" + jobId,
-        options
-      );
-      const data = await response.json();
-
-      const skeletonElements = document.querySelectorAll("[ms-code-skeleton]");
-
-      skeletonElements.forEach((element) => {
-        // Get delay from the attribute
-        let delay = element.getAttribute("ms-code-skeleton");
-
-        // If attribute value is not a number, set default delay as 2000ms
-        if (isNaN(delay)) {
-          delay = 2000;
-        }
-
-        setTimeout(() => {
-          // Remove the skeleton loader div after delay
-          const skeletonDiv = element.querySelector(".skeleton-loader");
-          element.removeChild(skeletonDiv);
-        }, delay);
-      });
-
-      return data;
-    } catch (error) {
-      return [];
-    }
-  };
-
-  const job = await fetchJob();
-  console.log(job);
-
-  document.querySelector('#jobDetail [data-element="job-title"]').textContent =
-    job.title;
-  document.querySelector(
-    '#jobDetail [data-element="job-address"]'
-  ).textContent = job.address;
-  document.querySelector('#jobDetail [data-element="job-city"]').textContent =
-    job.city;
-  document.querySelector(
-    '#jobDetail [data-element="job-description"]'
-  ).innerHTML = job.description;
-  document.querySelector('#jobDetail [data-element="owner"]').textContent =
-    job.owners[0].name;
-  // document.querySelector('#jobDetail [data-element="owner-photo"]').setAttribute("src", job.owners[0].avatar_original_url);
-  document.querySelector('#jobDetail [data-element="email"]').textContent =
-    job.owners[0].email;
-  document.querySelector('#jobDetail [data-element="phone"]').textContent =
-    job.owners[0].phone;
-  document.querySelector(
-    '#jobDetail [data-element="job-category"]'
-  ).textContent = job.category === null ? "Others" : job.category.name;
-  document.querySelector(
-    '#jobDetail [data-element="job-company"]'
-  ).textContent = job.company === null ? "-" : job.company.name;
-  document.querySelector('#jobDetail [data-element="job-type"]').textContent =
-    job.job_type.name;
-  document.querySelector('#jobDetail [data-element="job-salary"]').textContent =
-    job.salary;
-  document.querySelector('#jobDetail [data-element="job-date"]').textContent =
-    moment(job.published_at).startOf("day").fromNow();
-  document.querySelector(
-    '#jobDetail [data-element="job-due-date"]'
-  ).textContent = job.published_end_date
-    ? moment(job.published_end_date).format("LL")
-    : "-";
 };
+
+const job = await fetchJob();
+console.log(job);
+
+document.querySelector('#jobDetail [data-element="job-title"]').textContent =
+  job.title;
+document.querySelector('#jobDetail [data-element="job-address"]').textContent =
+  job.address;
+document.querySelector('#jobDetail [data-element="job-city"]').textContent =
+  job.city;
+document.querySelector(
+  '#jobDetail [data-element="job-description"]'
+).innerHTML = job.description;
+document.querySelector('#jobDetail [data-element="owner"]').textContent =
+  job.owners[0].name;
+// document.querySelector('#jobDetail [data-element="owner-photo"]').setAttribute("src", job.owners[0].avatar_original_url);
+document.querySelector('#jobDetail [data-element="email"]').textContent =
+  job.owners[0].email;
+document.querySelector('#jobDetail [data-element="phone"]').textContent =
+  job.owners[0].phone;
+document.querySelector('#jobDetail [data-element="job-category"]').textContent =
+  job.category === null ? "Others" : job.category.name;
+document.querySelector('#jobDetail [data-element="job-company"]').textContent =
+  job.company === null ? "-" : job.company.name;
+document.querySelector('#jobDetail [data-element="job-type"]').textContent =
+  job.job_type.name;
+document.querySelector('#jobDetail [data-element="job-salary"]').textContent =
+  job.salary;
+document.querySelector('#jobDetail [data-element="job-date"]').textContent =
+  moment(job.published_at).startOf("day").fromNow();
+document.querySelector('#jobDetail [data-element="job-due-date"]').textContent =
+  job.published_end_date ? moment(job.published_end_date).format("LL") : "-";
 
 // Apply Job Function
 document.addEventListener("DOMContentLoaded", function () {
